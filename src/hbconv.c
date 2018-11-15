36d4f1483f79639010f983dc96602965f1a08cdc
gromoff97 <gromoff97@mail.ru>
2018-11-15 21:29:54 +0300

    loop was put in code ("for"-statement was chosen)

--- src/hbconv.c
+++ src/hbconv.c
@@ -5,6 +5,14 @@
 
 int main ( int argc, char const *argv[] )
 {
-	if ( ARG_COUNT != argc ) return -1; 
+	size_t char_counter, char_count;
+	if ( ARG_COUNT != argc ) return -1;
+
+	char_count = strlen(argv[1]);
+	for ( char_counter = 0 ; char_counter < char_count; char_counter++ )
+	{
+		/* fprintf(stdout, "%c\n", argv[1][char_counter]); */
+	}
+
 	return 0;
 }

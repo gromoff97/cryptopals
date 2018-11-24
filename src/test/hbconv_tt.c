13541153c3b6b4b2527ea220dd355407a9af7f4e
gromoff97 <gromoff97@mail.ru>
2018-11-24 21:06:59 +0300

    simple data for testing was created

--- src/test/hbconv_tt.c
+++ src/test/hbconv_tt.c
@@ -4,10 +4,38 @@
 
 int main ( void )
 {
-	char test_data[][16] = {
-		{"abcdef"},
-		{"01234abcd"},
-		{"0"},
+	char test_data[][17] = {
+		{"5"},
+		{"42"},
+		{"78a"},
+		{"d34b"},
+		{"08e49"},
+		{"3b4d82"},
+		{"d4befb4"},
+		{"b5c995c1"},
+		{"7c9f5a8a4"},
+		{"273aca398e1"},
+		{"31f1f458e441"},
+		{"b8bbc35ef21fd"},
+		{"2fc75b8954e8df"},
+		{"7b69666116c332ac"}
+	};
+
+	char test_data_answers[][13] = {
+		{"BQ=="},
+		{"Qg=="},
+		{"B4o="},
+		{"00s="},
+		{"AI5J"},
+		{"O02C"},
+		{"DUvvtA=="},
+		{"tcmVwQ=="},
+		{"B8n1qKQ="},
+		{"AnOso5jh"},
+		{"MfH0WORB"},
+		{"C4u8Ne8h/Q=="},
+		{"L8dbiVTo3w=="},
+		{"e2lmYRbDMqw="}
 	};
 
 	size_t string_counter = 0;

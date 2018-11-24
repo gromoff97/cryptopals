686066350e38b04dab640ac6b749ba44d942fd9b
gromoff97 <gromoff97@mail.ru>
2018-11-24 20:48:15 +0300

    loop for checking string results was created

--- src/test/hbconv_tt.c
+++ src/test/hbconv_tt.c
@@ -13,5 +13,11 @@ int main ( void )
 	size_t string_counter = 0;
 	size_t string_count = sizeof(test_data) / sizeof(*test_data);
 
+	while ( string_counter < string_count )
+	{
+		/* assertions will be here */
+		string_counter++;
+	}
+
 	return 0;
 }
